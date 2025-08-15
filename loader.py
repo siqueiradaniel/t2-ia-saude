c729259fa8267de5df2580cc614601381539a185
siqueiradaniel <danielsiqueira111@gmail.com>
2025-08-15 10:08:21 -0300

    dataloader almost done

--- loader.py
+++ loader.py
@@ -8,194 +8,168 @@ import torchvision.transforms as transforms
 import torchvision.transforms.functional as F
 from torch.utils.data import DataLoader
 
+def my_collate_fn(batch):
+    images, labels, meta_data, uids = zip(*batch)
+    images = torch.stack(images, 0)          # empilha imagens
+    return images, list(labels), list(meta_data), list(uids)
+
+
 class MyDataset (data.Dataset):
-    def __init__(self, imgs_path, csv_path, dicom_info_filename, mass_train_filename, calc_train_filename, mass_test_filename, calc_test_filename, transform=None):
-        
+    def __init__(self, imgs_path, labels, meta_data=None, transform=None):
+        """
+        The constructor gets the images path and their respectively labels and meta-data (if applicable).
+        In addition, you can specify some transform operation to be carry out on the images.
+
+        It's important to note the images must match with the labels (and meta-data if applicable). For example, the
+        imgs_path[x]'s label must take place on labels[x].
+
+        Parameters:
+        :param imgs_path (list): a list of string containing the image paths
+        :param labels (list) a list of labels for each image
+        :param meta_data (list): a list of meta-data regarding each image. If None, there is no information.
+        Defaul is None.
+        :param transform (torchvision.transforms.Compose): transform operations to be carry out on the images
+        """
+
         super().__init__()
-        '''
-        patients: [[{
-            'SeriesInstanceUID': string,
-            'image_path': string,
-            'pathology': string,
-            'Laterality': string,
-            'SeriesDescription': string,
-            'PatientOrientation': string,
-            'abnormality type': string
-        }]]
-        '''
-        self.patients = []
-
-        ### Load CSV
-        dicom_info = pd.read_csv(os.path.join (csv_path, dicom_info_filename))
-        mass_train = pd.read_csv (os.path.join (csv_path, mass_train_filename))
-        calc_train = pd.read_csv (os.path.join (csv_path, calc_train_filename))
-        mass_test = pd.read_csv (os.path.join (csv_path, mass_test_filename))
-        calc_test = pd.read_csv (os.path.join (csv_path, calc_test_filename))
-
-        # Rename columns to a pattern
-        mass_train.rename (columns= {"patient_id" : "patient id", "breast_density" : "breast density"}, inplace=True)
-        calc_train.rename (columns= {"patient_id" : "patient id"}, inplace=True)
-        mass_test.rename (columns= {"patient_id" : "patient id", "breast_density" : "breast density"}, inplace=True)
-        calc_test.rename (columns= {"patient_id" : "patient id"}, inplace=True)
-
-        ### Remove ROI mask
-        dicom_info = dicom_info[dicom_info["SeriesDescription"] != "ROI mask images"]
-
-        ### Concat dataframes
-        all_data = pd.concat([mass_train, calc_train, mass_test, calc_test], ignore_index=True)
-
-        ### Merge dicom_info e all_data
-        # Cria colunas para Merge
-        dicom_info["SeriesInstanceUID2"] = dicom_info["SeriesInstanceUID"]
-        dicom_info["SeriesInstanceUID1"] = dicom_info["SeriesInstanceUID"]
-        all_data["SeriesInstanceUID1"] = all_data["image file path"].str.split('/').str[2]
-        all_data["SeriesInstanceUID2"] = all_data["cropped image file path"].str.split('/').str[2]
-
-        # Merge usando SeriesInstanceUID1
-        merge1 = pd.merge(dicom_info, all_data, left_on="SeriesInstanceUID1", right_on="SeriesInstanceUID1", how='inner')
-
-        # Merge usando SeriesInstanceUID2
-        merge2 = pd.merge(dicom_info, all_data, left_on="SeriesInstanceUID2", right_on="SeriesInstanceUID2", how='inner')
-
-        # Concatenar os dois resultados e remover duplicatas
-        result = pd.concat([merge1, merge2], ignore_index=True).drop_duplicates()
-
-        ### Preenche self.patients
-        # Agrupa pelo paciente
-        for _, group in result.groupby('patient id'):
-            imgs = []
-            for _, row in group.iterrows():
-                img_data = {
-                    'SeriesInstanceUID': row.get('SeriesInstanceUID', None),
-                    'image_path': row.get('image_path', None),
-                    'pathology': row.get('pathology', None),
-                    'Laterality': row.get('Laterality', None),
-                    'SeriesDescription': row.get('SeriesDescription', None),
-                    'PatientOrientation': row.get('PatientOrientation', None),
-                    'abnormality type': row.get('abnormality type', None)
-                }
-                imgs.append(img_data)
-        self.patients.append(imgs)
-
         self.imgs_path = imgs_path
+        self.labels = labels 
+        self.meta_data = meta_data 
+
+        # if transform is None, we need to ensure that the PIL image will be transformed to tensor, otherwise we'll get
+        # an exception
         if transform is not None:
             self.transform = transform
         else:
             self.transform = transforms.ToTensor()
 
+     
+        
+
     def __len__(self):
-        """Total de imagens no dataset (não apenas pacientes)"""
-        return sum(len(p) for p in self.patients)
+        """ This method just returns the dataset size """
+        return len(self.imgs_path)
 
-    def __getitem__(self, idx):
+
+    def __getitem__(self, item):
         """
-        Retorna uma única imagem, rótulo e metadados a partir de um índice linear.
-        """
-
-        # Map index to patient and image index
-        count = 0
-        for patient in self.patients:
-            if idx < count + len(patient):
-                image_data = patient[idx - count]
-                break
-            count += len(patient)
-        else:
-            raise IndexError("Index out of range")
-
-        # Carrega a imagem
-        image = Image.open(os.path.join(self.imgs_path, image_data['image_path'])).convert("RGB")
-
-        if self.transform is not None:
-            image = self.transform(image)
-
-        label = image_data['pathology']
-        meta_data = image_data
-        uid = image_data['SeriesInstanceUID']
-
-        return image, label, meta_data, uid
-
-    
-    def get_patients(self):
-        return self.patients
-
-    def get_image_by_patient(self, patient_index, image_index):
-        """
-        It gets the image, labels and meta-data (if applicable) according to the index informed in `image_index`.
+        It gets the image, labels and meta-data (if applicable) according to the index informed in `item`.
         It also performs the transform on the image.
 
-        :param image_index (int): an index in the interval [0, ..., len(img_paths)-1]
+        :param item (int): an index in the interval [0, ..., len(img_paths)-1]
         :return (tuple): a tuple containing the image, its label and meta-data (if applicable)
         """
 
-        image = Image.open(os.path.join (self.imgs_path, self.patients[patient_index][image_index]['image_path'])).convert("RGB")
-            
-        if self.transform is not None:
-            image = self.transform(image)
+        image = Image.open(self.imgs_path[item]).convert("RGB")
 
-        img_id = self.patients[patient_index][image_index]['SeriesInstanceUID']      
-        labels = self.patients[patient_index][image_index]['pathology']
-        meta_data = self.patients[patient_index][image_index]
+        # Applying the transformations
+        image = self.transform(image)
+
+        img_id = self.imgs_path[item].split('/')[-1].split('.')[0]
+
+        if self.meta_data is None:
+            meta_data = []
+        else:
+            meta_data = self.meta_data[item]
+
+        if self.labels is None:
+            labels = []
+        else:
+            labels = self.labels[item]
 
         return image, labels, meta_data, img_id
 
-def get_data_loader(imgs_path,
-                    csv_path,
-                    dicom_info_filename,
-                    mass_train_filename,
-                    calc_train_filename,
-                    mass_test_filename,
-                    calc_test_filename,
-                    transform=None,
-                    batch_size=30,
-                    shuffle=True,
-                    num_workers=4,
-                    pin_memory=True):
+def get_data_loader (imgs_path, labels, meta_data=None, transform=None, batch_size=30, shuf=True, num_workers=4,
+                     pin_memory=False):
     """
-    Cria um DataLoader a partir do dataset CBIS-DDSM com as transformações e parâmetros especificados.
-    
-    :return: torch.utils.data.DataLoader
+    This function gets a list og images path, their labels and meta-data (if applicable) and returns a DataLoader
+    for these files. You also can set some transformations using torchvision.transforms in order to perform data
+    augmentation. Lastly, params is a dictionary that you can set the following parameters:
+    batch_size (int): the batch size for the dataset. If it's not informed the default is 30
+    shuf (bool): set it true if wanna shuffe the dataset. If it's not informed the default is True
+    num_workers (int): the number thread in CPU to load the dataset. If it's not informed the default is 0 (which
+
+
+    :param imgs_path (list): a list of string containing the images path
+    :param labels (list): a list of labels for each image
+    :param meta_data (list, optional): a list of meta-data regarding each image. If it's None, it means there's
+    no meta-data. Default is None
+    :param transform (torchvision.transforms, optional): use the torchvision.transforms.compose to perform the data
+    augmentation for the dataset. Alternatively, you can use the jedy.pytorch.utils.augmentation to perform the
+    augmentation. If it's None, none augmentation will be perform. Default is None
+    :param batch_size (int): the batch size. If the key is not informed or params = None, the default value will be 30
+    :param shuf (bool): if you'd like to shuffle the dataset. If the key is not informed or params = None, the default
+    value will be True
+    :param num_workers (int): the number of threads to be used in CPU. If the key is not informed or params = None, the
+    default value will be  4
+    :param pin_memory (bool): set it to True to Pytorch preload the images on GPU. If the key is not informed or
+    params = None, the default value will be True
+    :return (torch.utils.data.DataLoader): a dataloader with the dataset and the chose params
     """
 
-    dataset = MyDataset(
-        imgs_path=imgs_path,
-        csv_path=csv_path,
-        dicom_info_filename=dicom_info_filename,
-        mass_train_filename=mass_train_filename,
-        calc_train_filename=calc_train_filename,
-        mass_test_filename=mass_test_filename,
-        calc_test_filename=calc_test_filename,
-        transform=transform
-    )
+    dt = MyDataset(imgs_path, labels, meta_data, transform)
+    dl = DataLoader (dataset=dt, batch_size=batch_size, shuffle=shuf, num_workers=num_workers,
+                          pin_memory=pin_memory, collate_fn=my_collate_fn)
+    return dl
 
-    dataloader = DataLoader(
-        dataset=dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        num_workers=num_workers,
-        pin_memory=pin_memory
-    )
 
-    return dataloader
 
+### Pegar csv e passar listas para o Dataset
+### Load CSV
+csv_path="./csv/"
+dicom_info_filename="dicom_info.csv"
+mass_train_filename="mass_case_description_train_set.csv"
+calc_train_filename="calc_case_description_train_set.csv"
+mass_test_filename="mass_case_description_test_set.csv"
+calc_test_filename="calc_case_description_test_set.csv"
+
+dicom_info = pd.read_csv(os.path.join (csv_path, dicom_info_filename))
+mass_train = pd.read_csv (os.path.join (csv_path, mass_train_filename))
+calc_train = pd.read_csv (os.path.join (csv_path, calc_train_filename))
+mass_test = pd.read_csv (os.path.join (csv_path, mass_test_filename))
+calc_test = pd.read_csv (os.path.join (csv_path, calc_test_filename))
+
+# Rename columns to a pattern
+mass_train.rename (columns= {"patient_id" : "patient id", "breast_density" : "breast density"}, inplace=True)
+calc_train.rename (columns= {"patient_id" : "patient id"}, inplace=True)
+mass_test.rename (columns= {"patient_id" : "patient id", "breast_density" : "breast density"}, inplace=True)
+calc_test.rename (columns= {"patient_id" : "patient id"}, inplace=True)
+
+### Remove full mammogram images
+dicom_info = dicom_info[dicom_info["SeriesDescription"] != "full mammogram images"]
+
+### Concat dataframes
+train_data_csv = pd.concat([mass_train, calc_train], ignore_index=True)
+test_data_csv = pd.concat([mass_test, calc_test], ignore_index=True)
+
+### Merge dicom_info e all_data
+# Cria colunas para Merge
+dicom_info["SeriesInstanceUID2"] = dicom_info["SeriesInstanceUID"]
+dicom_info["SeriesInstanceUID1"] = dicom_info["SeriesInstanceUID"]
+train_data_csv["SeriesInstanceUID1"] = train_data_csv["image file path"].str.split('/').str[2]
+train_data_csv["SeriesInstanceUID2"] = train_data_csv["cropped image file path"].str.split('/').str[2]
+
+# Merge usando SeriesInstanceUID1
+merge1 = pd.merge(dicom_info, train_data_csv, left_on="SeriesInstanceUID1", right_on="SeriesInstanceUID1", how='inner')
+
+# Merge usando SeriesInstanceUID2
+merge2 = pd.merge(dicom_info, train_data_csv, left_on="SeriesInstanceUID2", right_on="SeriesInstanceUID2", how='inner')
+
+# Concatenar os dois resultados e remover duplicatas
+train_csv_merged = pd.concat([merge1, merge2], ignore_index=True).drop_duplicates()
+
+# Transform
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
 
-loader = get_data_loader(
-    imgs_path="./",
-    csv_path="./csv/",
-    dicom_info_filename="dicom_info.csv",
-    mass_train_filename="mass_case_description_train_set.csv",
-    calc_train_filename="calc_case_description_train_set.csv",
-    mass_test_filename="mass_case_description_test_set.csv",
-    calc_test_filename="calc_case_description_test_set.csv",
-    transform=transform,
-    batch_size=16,
-    shuffle=True,
-    num_workers=2,
-    pin_memory=True
-)
+imgs_path = list(train_csv_merged['image_path'])
+labels = list(train_csv_merged['pathology'])
+meta_data = train_csv_merged.drop(columns=['image_path', 'pathology']).to_dict(orient='records')
+
+# Create DataLoader
+loader = get_data_loader (imgs_path, labels, meta_data, transform)
 
 # Pegando um batch
 for images, labels, meta_data, uids in loader:
